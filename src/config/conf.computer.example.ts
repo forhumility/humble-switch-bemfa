1c6951b5ec1b884ad4b0decd3563f537b3aad163
humility <793039998@qq.com>
2023-07-25 10:29:11 +0800

    docs: 添加配置示例 07-25

---
+++ src/config/conf.computer.example.ts
@@ -0,0 +1,27 @@
+/**
+ * @Author       : Humility
+ * @Date         : 2023-07-12 14:12:46
+ * @LastEditTime : 2023-07-13 11:09:58
+ * @LastEditors  : Humility
+ * @FilePath     : \miot-pc-switch-bemfa\src\config\conf.computer.ts
+ * @Description  : 电脑配置
+ */
+
+export interface ComputerInfo {
+  name: string;
+  theme: string;
+  ip: string;
+  mac: string;
+  user: string;
+  password: string;
+}
+export const computerList: Array<ComputerInfo> = [
+  {
+    name: "我的电脑",
+    theme: "巴法云的主题",
+    ip: "局域网地址",
+    mac: "电脑mac地址",
+    user: "用户名",
+    password: "密码",
+  },
+];
